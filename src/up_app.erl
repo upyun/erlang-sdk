589c4ef9a6b13595df5b80d3258665c3f294be99
kaidi.ren <kaidi.ren@upai.com>
2015-02-05 14:46:34 +0800

    let it be otp application

---
+++ src/up_app.erl
@@ -0,0 +1,19 @@
+-module(up_app).
+
+-behaviour(application).
+
+-export([
+     start/2,
+     stop/1
+     ]).
+
+start(_Type, _StartArgs) ->
+    case up_sup:start_link() of
+      {ok, Pid} ->
+          {ok, Pid};
+      Other ->
+          {error, Other}
+    end.
+
+stop(_State) ->
+    ok.
